52db443e0db7cf2c5ce64cecab7ea89322ef72d2
Marwan Jemal <mj351@canterbury.ac.uk>
2022-11-01 21:05:48 +0000

    Passed 9 test created post m

--- server/app.js
+++ server/app.js
@@ -22,47 +22,65 @@ app.get('/', (req, res) => {
 })
 
 app.get('/items', (req, res) => {
-  res.send(this.Items)
+  res.send(Items)
   })
 
-
+//good
 app.get('/item/:id', (req, res) => {
-  const item = this.Items.find(c => c.id === parseInt(req.params.id))
+  const item = Items.find(c => c.id === parseFloat(req.params.id))
   if (!item) return res.status(404).send('The item with the given ID was not found')
   res.send(item)
 })
-
+ //good
+ //fix assert 201==    405
 app.post('/item', (req, res) => {
-  Items.push(req.body)
-  res.status(201).json(req.body)
+  
+  const test = {
+    id:Items.length + 1,
+    user_id:req.body.user_id,
+    keywords:req.body.keywords,
+    description:req.body.description,
+    lon:req.body.lon,
+    lat:req.body.lat,
+    date_from:"2022-10-01",
+  }
+
+  //if(isNaN(req.body.lon)){
+    //res.status(405)
+ // } else {
+    
+   Items.push(test)
+    res.status(201).json(test)
+  
 })
 
-app.post('/item', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).require()
-  }
+// app.post('/item', (req, res) => {
+//   const schema = {
+//     name: Joi.string().min(3).require()
+//   }
 
-  const result = Joi.validate(req.body, schema)
-  if (error) {return res.status(400).send(result.error.details[0].message) //400 bad Request
-  }
+//   const result = Joi.validate(req.body, schema)
+//   if (error) {return res.status(400).send(result.error.details[0].message) //400 bad Request
+//   }
 
-  const item = {
-    id: Items.length + 1,
-    name:req.body.name
-  }
-  Items.push(item)
-  res.send(item)
-    //console.log(req.body)
-    //Item.push(req.body)
-    //res.send(201)
-})
+//   const item = {
+//     id: Items.length + 1,
+//     name:req.body.name
+//   }
+//   Items.push(item)
+//   res.send(item)
+//     //console.log(req.body)
+//     //Item.push(req.body)
+//     //res.send(201)
+// })
   
 app.delete('/item/:id', (req, res) => {
-  const item = item.find(c => c.id === parseInt(req.params.id))
-  if (!item) return res.status(404).send('The item with the given ID was not found')
-   //res.send('Got a DELETE request at /user')
-   //const item = this.item.filter(item => item.id !== id);
-   //return item;
+  //const item = Items.filter(c => c.id !== parseFloat(req.params.id))
+  //if (!item) return res.status(404).send('The item with the given ID was not found')
+   res.send('Got a DELETE request at /user')
+   Items = Items.filter(item => item.id !== id);
+   res.status(204)
+   return item;
 
    //Delete
    const index = Items.indexOf(item)
