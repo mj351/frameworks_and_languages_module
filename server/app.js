1c41999a1e41b12db041ce2fb309bbf27a163cc7
Marwan Jemal <mj351@canterbury.ac.uk>
2022-11-14 05:58:52 +0000

    Mongoose.schema

--- server/app.js
+++ server/app.js
@@ -1,26 +1,66 @@
-const express = require('express')      //require express library 
-const app = express()                  //create an application 
-const port = 8000
-const itemRouter = require ('./routes/item.js')
-const ITEM = require('./itemsD')
-//const { body } = require('express-validator')
-var cors = require('cors')
+const express = require('express');
+const app = express();
+const morgan = require ('morgan');
+const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
+const port = 8000;
 
-app.use(express.json()) //Enable json
-app.use('./item.js', itemRouter)
+const itemRouter = require ('./routes/item');
+const itemsRouter = require ('./routes/items');
 
-/*app.get('/', (req, res) => {
-  return res.status(200).send('<html><body>Hello world</body></html>')
-})*/
+mongoose.connect('mongodb+srv://mj351:' + process.env.MONGO_ATLAS_PW + '@cluster0.jytfghq.mongodb.net/?retryWrites=true&w=majority',
+{
+  useMongoClient: true
+});
 
-app.options('*', cors())
+//var cors = require('cors');
+
+//app.use(express.json()) //Enable json
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next)=> {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods','GET, POST, DELETE');
+    return res.status(200).json({});
+  }
+  
+});
+
+// Routes which should handle requests
+app.use('./item', itemRouter);
+app.use('./items', itemsRouter);
+
+
+app.use((req, res, next)=>{
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, res, next)=>{
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+//app.options('*', cors())
 
 // Serve................................................
 app.listen(port, () => {
   console.log(`Example app listening on port 8000 ${port}`)
 });
 
-module.exports = app;
-
 // Docker container exit handler - https://github.com/nodejs/node/issues/4182
-process.on('SIGINT', function() {process.exit()})
+process.on('SIGINT', function() {process.exit()});
+
+module.exports = app;
