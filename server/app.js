43ea440020b370c24473d4ce695fc4bdc3ecf093
Marwan Jemal <mj351@canterbury.ac.uk>
2022-11-21 16:31:18 +0000

    dockerfile changes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8000
-const ITEMS = require('./ITEMS');
+const ITEMS = require('./items');
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
