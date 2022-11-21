70bbae97e24f66e06f1d01222467a4f4b4f0bf0b
Marwan Jemal <mj351@canterbury.ac.uk>
2022-11-21 16:34:31 +0000

    add file

---
+++ server/items.js
@@ -0,0 +1,13 @@
+
+ITEMS = {
+    0: {
+      'id': 3,
+      'user_id': "1234",
+      'keywords': ["hammer", "nails", "tools"],
+      "description": "A hammer and nails set. In canterbury",
+      "lat": 51.2798438,
+      "lon": 1.0830275,
+      "date_from": "2021-11-22T08:22:39.067408"
+    },
+  }
+module.exports = ITEMS;
