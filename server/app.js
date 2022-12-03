241ea938e988e91f5106762f0fcca41e1cf2d525
Marwan Jemal <mj351@canterbury.ac.uk>
2022-12-03 15:18:34 +0000

    add index.html

--- server/app.js
+++ server/app.js
@@ -32,6 +32,7 @@ app.post('/item', (req, res) => {
       user_id:req.body.user_id,
       keywords:req.body.keywords,
       description:req.body.description,
+      image:req.body.image,
       lon:req.body.lon,
       lat:req.body.lat,
       date_from: newDate,
@@ -45,24 +46,14 @@ app.post('/item', (req, res) => {
 })
 
 // GET
-/*app.get('item/:id', (req, res) => {
-  const id = re.params.itemId;
-  if (id === 'special') {
-    res.status(200).json({
-      message: 'You discovered the special ID',
-      id: id
-    });
-  } else {
-     res.status(200).json({
-      message: 'You passed an ID'
-     });
+app.get('item/:id', (req, res) => {
+  const id = req.params.id;
+  const item = ITEMS.find((item) => item.id === id);
+  if (item) {
+    res.json(item);
+  } else{
+    res.status(404).send('The item with the given ID was not found')
   }
-});*/
-
-app.get('/item/:id', (req, res) => {
-   const item = ITEMS.find(c => c.id === parseFloat(req.params.user_id))
-   if (!item) return res.status(404).send('The item with the given ID was not found')
-   res.send(item)
 })
 
 //Get Items 
@@ -78,47 +69,11 @@ app.get('/items', (req, res) => {
 })
 
 // DELETE 
-
 app.delete('item/:id', (req, res) => {
-  const item = ITEMS.find(c => c.id === parseFloat(req.params.user_id))//look up at items
-  ITEMS.remove({ id: item })
-  .exec()
-  .then(result => {
-    res.status(204).json(result);
-  })
-  .catch(err =>{
-    console.log(err);
-    res.status(404).json({
-      error: err
-    });
-  });
+  const id = req.params.id;
+  ITEMS = ITEMS.find((item) => item.id !==id);
+  res.send('item with id ${id} has been deleted')
 });
-
-/*
-app.delete('/item/:id', (req, res) => {
-  const item = ITEMS.find(c => c.id === parseFloat(req.params.id)) //look up at items
-  if (!item) return res.status(404).send('The item with the given ID was not found') //if not existing, return 404
-
-  const index = ITEMS.indexOf(item)
-  ITEMS.splice(index, 1)
-  res.status(204).send(item)
-})*/
-
-/*
-app.use((req, res,  ) => {
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-})
-
-app.use((error, res, next)=>{
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
-})*/
 
 // Serve................................................
 app.listen(port, () => {
