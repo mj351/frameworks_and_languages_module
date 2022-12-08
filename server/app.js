30817444adb1bfe3b874a1c70f1be0deaf2ede3c
Marwan Jemal <mj351@canterbury.ac.uk>
2022-12-08 09:31:58 +0000

    ITEMS to items and test_client post

--- server/app.js
+++ server/app.js
@@ -1,32 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 8000
-const ITEMS = require('./items')
+const items = require('./items')
+const cors = require('cors')
 
 // Init Express ----------------------
 app.use(express.json());// Enable json input from incoming requests. This is accessible from `req.body`
-
-// CORS - https://expressjs.com/en/resources/middleware/cors.html
-const cors = require('cors')
+app.use(express.urlencoded())
+//CORS - https://expressjs.com/en/resources/middleware/cors.html
 app.use(cors({
 }))
 
-// Routes ----------------------
+
+//Routes ----------------------
 app.get('/',(req, res) => {
   res.status(200).send('<html><body>Hello World</body></html>')
 })
 
-// POST
+//GET itemId req
+app.get('/item/:id', (req, res) => {
+  itemId = parseInt(req.params.id)
+  if(items.hasOwnProperty(itemId)){
+    res.json(items[itemId])
+  }
+  else{
+    res.status(404).send("The item with the given ID was not found")
+  }
+})
+
+//POST Item req
 app.post('/item',(req, res) => {
 
-  const newItem = Object.keys(ITEMS).length +1
+  const newItem = Object.keys(items).length +1
   const newDate = new Date().toJSON().slice(0,10)
   
-  if(ITEMS.hasOwnProperty(newItem)){
+  if(items.hasOwnProperty(newItem)){
     newItem = newItem + 1
   }
   if(req.body.user_id && req.body.keywords && req.body.description && req.body.lat && req.body.lon !==""){
-    ITEMS[newItem] = {
+    items[newItem] = {
       id:newItem,
       user_id:req.body.user_id,
       keywords:req.body.keywords,
@@ -37,53 +49,30 @@ app.post('/item',(req, res) => {
       date_from: newDate,
       date_to: newDate
     }
-    res.status(201).json(ITEMS[newItem])
+    res.status(201).json(items[newItem])
   } 
   else {
     res.status(405).json('The item with the given ID was not found')
   }
 })
-
-// GET item/id
-app.get('item/:id', (req, res) => {
-  const id = parseFloat(req.params.id)
-  ITEMS = [...ITEMS.filter((item)=>item.id != id)]
-  res.status(204).json({})
-  
-  if (!ITEMS) {
-    res.status(404).send('The item with the given ID was not found')
-  }
-})
  
-//Get Items 
+//Get items req
 app.get('/items', (req, res) => {
   if (req.query.user_id) {
     res.status(200).json(
-      Object.values(ITEMS).filter(i => i.user_id == req.query.user_id))
+      Object.values(items).filter(i => i.user_id == req.query.user_id))
    return;
     }
     res.status(200).json(
-      Object.values(ITEMS))
+      Object.values(items))
 })
 
 // DELETE 
 app.delete('item/:id', (req, res) => {
   const id = parseFloat(req.params.id)
-  ITEMS = [...ITEMS.find((item) => item.id !=id)]
+  items = [...items.find((item) => item.id !=id)]
   res.status(204).json({})
 })
-
-/*/ https://expressjs.com/en/guide/error-handling.html
-function logErrors (err, req, res, next) {
-  console.error(err.stack)
-  next(err)
-}
-app.use(logErrors)
-app.use(function(req, res, next){
-  console.log('no route', req.originalUrl);
-  res.status(404).type('txt').send('Not found');
-  next()
-})*/
 
 // Serve ----------------------
 app.listen(port, () => {
